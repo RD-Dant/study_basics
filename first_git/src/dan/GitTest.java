e80c4b5b2dd494bae3757291d6c11afdc530ade4
Dant <dantuo@foxmail.com>
2022-03-20 18:27:20 +0800

    first commit by idea

---
+++ first_git/src/dan/GitTest.java
@@ -0,0 +1,13 @@
+package dan;
+
+/**
+ * @author DanTuo
+ * @version 1.0
+ * @date 2022/3/20 18:22
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
